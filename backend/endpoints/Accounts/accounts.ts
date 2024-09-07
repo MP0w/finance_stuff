b854c023939f7d56a3eba8a31f87be561938b39a
mpow <m@mpow.dev>
2024-09-07 23:13:39 +0800

    Accounts APIs

--- backend/endpoints/Accounts/accounts.ts
+++ backend/endpoints/Accounts/accounts.ts
@@ -1,19 +1,74 @@
-import { Application } from "express";
+import { Application, Request, Response } from "express";
+import { Accounts, Table } from "../../types";
+import { dbConnection, generateUUID } from "../../dbConnection";
 
 export function accountsRouter(app: Application) {
-  app.get("/account/:id", (req, res) => {
-    res.send(req.params["id"]);
+  app.get("/accounts", async (req, res) => {
+    const accounts = await dbConnection<Accounts>(Table.Accounts)
+      .select()
+      .where({ user_id: req.userId })
+      .limit(1000);
+
+    res.send(accounts);
   });
 
-  app.post("/entry", (_, res) => {
+  app.get("/account/:id", async (req, res) => {
+    const accounts = await dbConnection<Accounts>(Table.Accounts)
+      .select()
+      .where({ id: req.params.id, user_id: req.userId })
+      .limit(1);
+
+    const account = accounts.at(0);
+    if (!account) {
+      return res.status(404);
+    }
+
+    return res.send(account);
+  });
+
+  async function upsertAccount(id: string, req: Request, res: Response) {
+    const name = req.body.name;
+    const type = req.body.type;
+
+    if (!name || !type) {
+      throw Error("Invalid params");
+    }
+
+    await dbConnection<Accounts>(Table.Accounts).upsert({
+      id,
+      user_id: req.userId,
+      name,
+      type,
+      updated_at: new Date(),
+    });
+
     res.send({});
+  }
+
+  app.post("/account", async (req, res) => {
+    await upsertAccount(generateUUID(), req, res);
   });
 
-  app.put("/entry/:id", (_, res) => {
-    res.send({});
+  app.put("/account/:id", async (req, res) => {
+    const account = (
+      await dbConnection<Accounts>(Table.Accounts)
+        .select()
+        .where({ id: req.params.id })
+        .limit(1)
+    ).at(0);
+
+    if (!account || account.user_id !== req.userId) {
+      throw Error("invalid account");
+    }
+
+    await upsertAccount(req.params.id, req, res);
   });
 
-  app.delete("/entry/:id", (_, res) => {
+  app.delete("/account/:id", async (req, res) => {
+    await dbConnection<Accounts>(Table.Accounts)
+      .delete()
+      .where({ id: req.params.id, user_id: req.userId });
+
     res.send({});
   });
 }
--- backend/endpoints/Users/users.ts
+++ backend/endpoints/Users/users.ts
@@ -1,11 +1,24 @@
 import { Application } from "express";
+import { Table, Users } from "../../types";
+import { dbConnection } from "../../dbConnection";
 
 export function usersRouter(app: Application) {
-  app.get("/user/:id", (req, res) => {
-    res.send(req.params["id"]);
+  app.get("/user", async (req, res) => {
+    const users = await dbConnection<Users>(Table.Users)
+      .select()
+      .where({ id: req.userId })
+      .limit(1);
+
+    const user = users.at(0);
+    if (!user) {
+      throw Error("User not found");
+    }
+
+    res.send(user);
   });
 
   app.post("/sign-in", (req, res) => {
+    // todo auth
     res.send(req.body["token"]);
   });
 }
