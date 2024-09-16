83eeb27f429383e797f425c1e657b44943817f54
mpow <m@mpow.dev>
2024-09-16 12:33:19 +0800

    Sentry BE

--- backend/index.ts
+++ backend/index.ts
@@ -3,8 +3,17 @@ import express, { NextFunction, Request, Response } from "express";
 import { router } from "./endpoints/router";
 import { initializeApp } from "firebase-admin/app";
 import admin from "firebase-admin";
+import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 configDotenv({ path: ".env.local" });
+
+Sentry.init({
+  dsn: "https://c7970c79b7a58d700737f7a34fc57c8b@o4507960601214976.ingest.de.sentry.io/4507960663670864",
+  integrations: [nodeProfilingIntegration()],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
 
 const cert: admin.ServiceAccount = JSON.parse(process.env.FIREBASE_CERT!);
 
@@ -14,6 +23,8 @@ initializeApp({
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+Sentry.setupExpressErrorHandler(app);
 
 app.use(express.json());
 
