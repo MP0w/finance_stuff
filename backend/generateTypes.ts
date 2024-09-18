9429bc27157d4a38e75d5e8ad9a023cf57ede41a
mpow <m@mpow.dev>
2024-09-18 22:47:45 +0800

    Re-generate types

--- backend/generateTypes.ts
+++ backend/generateTypes.ts
@@ -1,6 +1,82 @@
 import { updateTypes } from "knex-types";
 import { dbConnection } from "./dbConnection";
+import { Transform, Writable } from "stream";
+import { createWriteStream } from "fs";
 
-updateTypes(dbConnection, { output: "./types.ts" }).catch(() => {
+let isFirst = true;
+let isAccountType = false;
+
+const customTypes = `
+export type AccountingEntriesDTO = AccountingEntries & { entries: Entries[] };
+
+export type AccountType = "fiat" | "investment";
+
+export type ConnectionsDTO = Omit<
+  Connections,
+  "settings" | "created_at" | "updated_at" | "user_id"
+>;
+
+export type Connector = {
+  id: string;
+  name: string;
+  type?: AccountType;
+  icon: string | undefined;
+  settings: {
+    key: string;
+    hint: string;
+    optional?: boolean;
+    extraInstructions?: string;
+    type: "string" | "number" | "boolean";
+  }[];
+};`;
+
+const transformer = new Transform({
+  transform(chunk, _, callback) {
+    if (isFirst) {
+      this.push("// This file is auto-generated. Do not edit it manually.\n\n");
+      this.push(customTypes);
+      isFirst = false;
+    }
+
+    const data = chunk.toString();
+
+    if (data.startsWith("export type Accounts = {")) {
+      isAccountType = true;
+    } else if (isAccountType && data.startsWith("};")) {
+      isAccountType = false;
+    }
+
+    if (data.includes("//")) {
+      this.push("\n");
+    } else if (data === "  type: string;\n") {
+      this.push("  type: AccountType;\n");
+    } else {
+      this.push(data);
+    }
+    callback();
+  },
+});
+
+const output = createWriteStream("./types.ts");
+
+const combinedStream = new Writable({
+  write(chunk, encoding, callback) {
+    transformer.write(chunk, encoding, () => {
+      output.write(transformer.read(), callback);
+    });
+  },
+});
+
+updateTypes(dbConnection, {
+  output: combinedStream,
+  exclude: ["knex_migrations", "knex_migrations_lock"],
+}).catch((error) => {
+  console.error("Error generating types:", error);
   process.exit(1);
+});
+
+// Ensure streams are properly closed when done
+combinedStream.on("finish", () => {
+  transformer.end();
+  output.end();
 });
--- backend/types.ts
+++ backend/types.ts
@@ -1,91 +1,9 @@
-// The TypeScript definitions below are automatically generated.
-// Do not touch them, or risk, your modifications being lost.
+// This file is auto-generated. Do not edit it manually.
 
-export enum Table {
-  AccountingEntries = "accounting_entries",
-  Accounts = "accounts",
-  Entries = "entries",
-  KnexMigrations = "knex_migrations",
-  KnexMigrationsLock = "knex_migrations_lock",
-  Users = "users",
-  Connections = "connections",
-}
-
-export type Tables = {
-  accounting_entries: AccountingEntries;
-  accounts: Accounts;
-  entries: Entries;
-  knex_migrations: KnexMigrations;
-  knex_migrations_lock: KnexMigrationsLock;
-  users: Users;
-  connections: Connections;
-};
-
-export type AccountingEntries = {
-  id: string;
-  user_id: string;
-  date: Date;
-  created_at: Date;
-  updated_at: Date;
-};
 
 export type AccountingEntriesDTO = AccountingEntries & { entries: Entries[] };
 
 export type AccountType = "fiat" | "investment";
-
-export type Accounts = {
-  id: string;
-  user_id: string;
-  name: string;
-  type: AccountType;
-  created_at: Date;
-  updated_at: Date;
-};
-
-export type Entries = {
-  id: string;
-  user_id: string;
-  account_id: string;
-  accounting_entry_id: string;
-  value: number;
-  invested: number | null;
-  created_at: Date;
-  updated_at: Date;
-};
-
-export type KnexMigrations = {
-  id: number;
-  name: string | null;
-  batch: number | null;
-  migration_time: Date | null;
-};
-
-export type KnexMigrationsLock = {
-  index: number;
-  is_locked: number | null;
-};
-
-export type Users = {
-  id: string;
-  firebase_uid: string;
-  email: string | null;
-  username: string | null;
-  name: string | null;
-  photo: string | null;
-  currency: string;
-  created_at: Date;
-  updated_at: Date;
-};
-
-export type Connections = {
-  id: string;
-  user_id: string;
-  connector_id: string;
-  account_id: string;
-  settings: string;
-  created_at: Date;
-  updated_at: Date;
-};
 
 export type ConnectionsDTO = Omit<
   Connections,
@@ -105,3 +23,70 @@ export type Connector = {
     type: "string" | "number" | "boolean";
   }[];
 };
+
+export enum Table {
+  AccountingEntries = "accounting_entries",
+  Accounts = "accounts",
+  Connections = "connections",
+  Entries = "entries",
+  Users = "users",
+}
+
+export type Tables = {
+  "accounting_entries": AccountingEntries,
+  "accounts": Accounts,
+  "connections": Connections,
+  "entries": Entries,
+  "users": Users,
+};
+
+export type AccountingEntries = {
+  id: string;
+  user_id: string;
+  date: Date;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export type Accounts = {
+  id: string;
+  user_id: string;
+  name: string;
+  type: AccountType;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export type Connections = {
+  id: string;
+  user_id: string;
+  account_id: string;
+  settings: string;
+  connector_id: string;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export type Entries = {
+  id: string;
+  user_id: string;
+  account_id: string;
+  accounting_entry_id: string;
+  value: number;
+  invested: number | null;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export type Users = {
+  id: string;
+  firebase_uid: string;
+  email: string | null;
+  username: string | null;
+  name: string | null;
+  photo: string | null;
+  currency: string;
+  created_at: Date;
+  updated_at: Date;
+};
+
