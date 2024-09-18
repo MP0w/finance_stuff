eb8577d51698e9f3137a9a1e38986b257f7320bc
mpow <m@mpow.dev>
2024-09-18 19:50:09 +0800

    Connectors list and delete

--- frontend/app/Home/ConnectorTab.tsx
+++ frontend/app/Home/ConnectorTab.tsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect, useMemo } from "react";
-import { Accounts, Connector } from "../../../backend/types";
+import { Accounts, Connector, ConnectionsDTO } from "../../../backend/types";
 import {
   useGetConnectorsSettings,
   useCreateConnection,
+  useGetConnections,
+  useDeleteConnection,
 } from "./apis/connectionsAPIs";
 import Image from "next/image";
 import toast from "react-hot-toast";
 import Link from "next/link";
+import Modal from "react-modal";
+import DeleteIcon from "../components/DeleteIcon";
 
 export const ConnectorsTab: React.FC<{
   accounts: Accounts[];
@@ -31,9 +35,31 @@ export const ConnectorsTab: React.FC<{
   );
   const [formData, setFormData] = useState<Record<string, string>>({});
 
+  const [connections, setConnections] = useState<ConnectionsDTO[]>([]);
+  const [connectionToDelete, setConnectionToDelete] =
+    useState<ConnectionsDTO | null>(null);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  const {
+    data: connectionsData,
+    loading: connectionsLoading,
+    error: connectionsError,
+    execute: fetchConnections,
+  } = useGetConnections();
+
+  const { execute: deleteConnection, loading: isDeleting } =
+    useDeleteConnection();
+
   useEffect(() => {
     getConnectorsSettings();
-  }, [getConnectorsSettings]);
+    fetchConnections();
+  }, [getConnectorsSettings, fetchConnections]);
+
+  useEffect(() => {
+    if (connectionsData) {
+      setConnections(connectionsData);
+    }
+  }, [connectionsData]);
 
   const handleAccountChange = (accountId: string) => {
     setSelectedAccount(accountId);
@@ -52,6 +78,10 @@ export const ConnectorsTab: React.FC<{
 
   const handleInputChange = (key: string, value: string) => {
     setFormData((prevData) => ({ ...prevData, [key]: value }));
+  };
+
+  const accountName = (id: string) => {
+    return accounts.find((account) => account.id === id)?.name;
   };
 
   const isFormValid = useMemo(() => {
@@ -92,126 +122,222 @@ export const ConnectorsTab: React.FC<{
     }
   };
 
+  const handleDeleteConnection = (connection: ConnectionsDTO) => {
+    setConnectionToDelete(connection);
+    setIsDeleteModalOpen(true);
+  };
+
+  const confirmDeleteConnection = async () => {
+    if (connectionToDelete) {
+      try {
+        await deleteConnection(connectionToDelete.id);
+        await fetchConnections();
+        toast.success("Connection deleted successfully", {
+          position: "bottom-right",
+        });
+      } catch (error) {
+        console.error("Error deleting connection:", error);
+        toast.error("Failed to delete connection. Please try again.", {
+          position: "bottom-right",
+        });
+      }
+    }
+    setIsDeleteModalOpen(false);
+    setConnectionToDelete(null);
+  };
+
   return (
     <div className="max-w-2xl mx-auto p-6">
-      <h2 className="text-2xl font-bold mb-6">Connect Account</h2>
-      {isLoading && <p>Loading connectors settings...</p>}
-      {error && <p>Error loading connectors settings: {error.message}</p>}
-      {!isLoading && !error && (
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <div>
-            <label
-              htmlFor="account"
-              className="block text-sm font-medium text-gray-700 mb-2"
-            >
-              Select an account:
-            </label>
-            <select
-              id="account"
-              value={selectedAccount}
-              onChange={(e) => handleAccountChange(e.target.value)}
-              required
-              className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-            >
-              <option value="">Choose an account</option>
-              {accounts.map((account, index) => (
-                <option key={index} value={account.id}>
-                  {account.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              {availableConnectors.length === 0
-                ? "No connectors available for this type of account, select another account"
-                : "Select a connector:"}
-            </label>
+      <h2 className="text-2xl font-bold mb-6">Connections</h2>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-              {availableConnectors.map((connector) => (
-                <button
-                  key={connector.id}
-                  type="button"
-                  onClick={() => setSelectedConnector(connector.id)}
-                  className={`flex flex-col items-center justify-center p-4 border rounded-md transition-colors ${
-                    selectedConnector === connector.id
-                      ? "bg-indigo-100 border-indigo-300"
-                      : "bg-white border-gray-300 hover:bg-gray-50"
-                  }`}
-                >
-                  {connector.icon && (
-                    <div className="m-2 rounded-xl border border-gray-300 overflow-hidden">
-                      <Image
-                        src={connector.icon}
-                        alt={connector.name}
-                        width={60}
-                        height={60}
-                        className="rounded-md"
-                      />
-                    </div>
-                  )}
-                  <span className="text-sm font-medium text-gray-900">
-                    {connector.name}
-                  </span>
-                </button>
-              ))}
-            </div>
-          </div>
-          {selectedConnector && (
-            <div className="mt-6">
-              <h3 className="text-lg font-medium text-gray-900 mb-4">
-                Connector Settings
-              </h3>
-              {availableConnectors
-                .find((c) => c.id === selectedConnector)
-                ?.settings.map((setting) => (
-                  <div key={setting.key} className="mb-4">
-                    <label
-                      htmlFor={setting.key}
-                      className="block text-md font-bold text-gray-800 mb-1"
-                    >
-                      {setting.hint}
-                      <p className="text-gray-600 text-sm font-normal">
-                        {setting.extraInstructions}
-                      </p>
-                    </label>
-                    <input
-                      type={setting.type === "number" ? "number" : "text"}
-                      id={setting.key}
-                      value={formData[setting.key] || ""}
-                      onChange={(e) =>
-                        handleInputChange(setting.key, e.target.value)
-                      }
-                      className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-                      required={setting.optional ? false : true}
-                    />
-                  </div>
-                ))}
-              <button
-                type="submit"
-                disabled={!isFormValid || isCreating}
-                className="w-full mt-8 flex justify-center py-2 px-4 border border-transparent shadow-sm text-md font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 pixel-corners-small disabled:opacity-50"
-              >
-                {isCreating ? "Connecting..." : "Connect"}
-              </button>
-            </div>
-          )}
-          {!selectedConnector && (
-            <p className="text-gray-600 text-sm font-normal">
-              <p className="mb-8">
-                If you don&apos;t see your favorite provider in the list, you
-                can send a feedback or email to us and we will consider adding
-                it!
-              </p>
-              <Link href="https://github.com/MP0w/finance_stuff_connectors">
-                You can also contribute, it&apos;s easy!
-                <p>https://github.com/MP0w/finance_stuff_connectors</p>
-              </Link>
-            </p>
-          )}
-        </form>
+      {connectionsLoading && <p>Loading connections...</p>}
+      {connectionsError && (
+        <p>Error loading connections: {connectionsError.message}</p>
       )}
+
+      {!connectionsLoading && !connectionsError && (
+        <>
+          {connections.length === 0 ? (
+            <p>No connections found. Add a new connector to get started.</p>
+          ) : (
+            <ul className="mb-6 space-y-4">
+              {connections.map((connection) => (
+                <li
+                  key={connection.id}
+                  className="flex items-center justify-between bg-white p-4 rounded-md shadow"
+                >
+                  <div>
+                    <p className="font-semibold">{connection.connector_id}</p>
+                    <p className="text-sm text-gray-600">
+                      ↪ {accountName(connection.account_id)}
+                    </p>
+                  </div>
+                  <button
+                    onClick={() => handleDeleteConnection(connection)}
+                    className="bg-transparent text-white px-3 py-1 rounded-md hover:bg-red-100"
+                  >
+                    <DeleteIcon className="text-red-500" />
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+        </>
+      )}
+
+      {
+        <div className="mt-8">
+          <h3 className="text-xl font-bold mb-4">Add New Connector</h3>
+          {isLoading && <p>Loading connectors settings...</p>}
+          {error && <p>Error loading connectors settings: {error.message}</p>}
+          {!isLoading && !error && (
+            <form onSubmit={handleSubmit} className="space-y-6">
+              <div>
+                <label
+                  htmlFor="account"
+                  className="block text-sm font-medium text-gray-700 mb-2"
+                >
+                  Select an account:
+                </label>
+                <select
+                  id="account"
+                  value={selectedAccount}
+                  onChange={(e) => handleAccountChange(e.target.value)}
+                  required
+                  className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                >
+                  <option value="">Choose an account</option>
+                  {accounts.map((account, index) => (
+                    <option key={index} value={account.id}>
+                      {account.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-2">
+                  {availableConnectors.length > 0 && "Select a connector:"}
+                  {availableConnectors.length === 0 &&
+                    selectedAccount &&
+                    "No connectors available for this type of account, select another account"}
+                </label>
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                  {availableConnectors.map((connector) => (
+                    <button
+                      key={connector.id}
+                      type="button"
+                      onClick={() => setSelectedConnector(connector.id)}
+                      className={`flex flex-col items-center justify-center p-4 border rounded-md transition-colors ${
+                        selectedConnector === connector.id
+                          ? "bg-indigo-100 border-indigo-300"
+                          : "bg-white border-gray-300 hover:bg-gray-50"
+                      }`}
+                    >
+                      {connector.icon && (
+                        <div className="m-2 rounded-xl border border-gray-300 overflow-hidden">
+                          <Image
+                            src={connector.icon}
+                            alt={connector.name}
+                            width={60}
+                            height={60}
+                            className="rounded-md"
+                          />
+                        </div>
+                      )}
+                      <span className="text-sm font-medium text-gray-900">
+                        {connector.name}
+                      </span>
+                    </button>
+                  ))}
+                </div>
+              </div>
+              {selectedConnector && (
+                <div className="mt-6">
+                  <h3 className="text-lg font-medium text-gray-900 mb-4">
+                    Connector Settings
+                  </h3>
+                  {availableConnectors
+                    .find((c) => c.id === selectedConnector)
+                    ?.settings.map((setting) => (
+                      <div key={setting.key} className="mb-4">
+                        <label
+                          htmlFor={setting.key}
+                          className="block text-md font-bold text-gray-800 mb-1"
+                        >
+                          {setting.hint}
+                          <p className="text-gray-600 text-sm font-normal">
+                            {setting.extraInstructions}
+                          </p>
+                        </label>
+                        <input
+                          type={setting.type === "number" ? "number" : "text"}
+                          id={setting.key}
+                          value={formData[setting.key] || ""}
+                          onChange={(e) =>
+                            handleInputChange(setting.key, e.target.value)
+                          }
+                          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                          required={setting.optional ? false : true}
+                        />
+                      </div>
+                    ))}
+                  <button
+                    type="submit"
+                    disabled={!isFormValid || isCreating}
+                    className="w-full mt-8 flex justify-center py-2 px-4 border border-transparent shadow-sm text-md font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 pixel-corners-small disabled:opacity-50"
+                  >
+                    {isCreating ? "Connecting..." : "Connect"}
+                  </button>
+                </div>
+              )}
+              {!selectedConnector && selectedAccount && (
+                <p className="text-gray-600 text-sm font-normal">
+                  <p className="mb-8">
+                    If you don&apos;t see your favorite provider in the list,
+                    you can send a feedback or email to us and we will consider
+                    adding it!
+                  </p>
+                  <Link href="https://github.com/MP0w/finance_stuff_connectors">
+                    You can also contribute, it&apos;s easy!
+                    <p>https://github.com/MP0w/finance_stuff_connectors</p>
+                  </Link>
+                </p>
+              )}
+            </form>
+          )}
+        </div>
+      }
+
+      <Modal
+        isOpen={isDeleteModalOpen}
+        onRequestClose={() => setIsDeleteModalOpen(false)}
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <h2 className="text-xl font-semibold mb-4">Confirm Deletion</h2>
+        <p className="mb-6">
+          Are you sure you want to delete the connection to
+          <b> {connectionToDelete?.connector_id}</b> for account
+          <b> {accountName(connectionToDelete?.account_id ?? "")}</b>? This
+          action cannot be undone.
+        </p>
+        <div className="flex justify-end">
+          <button
+            onClick={() => setIsDeleteModalOpen(false)}
+            className="bg-gray-300 text-gray-800 px-4 py-2 rounded-md mr-2 hover:bg-gray-400"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={confirmDeleteConnection}
+            className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
+            disabled={isDeleting}
+          >
+            {isDeleting ? "Deleting..." : "Delete"}
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 };
