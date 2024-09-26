df908c07588ff01b4d78f164d810026fde13107a
mpow <m@mpow.dev>
2024-09-27 00:13:37 +0800

    Adjust react strict mode

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: process.env.NEXT_PUBLIC_ENVIRONMENT === "local",
   images: {
     remotePatterns: [
       {
